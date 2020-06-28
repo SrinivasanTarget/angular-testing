4eeda9ba3baec0a0e0f99c364de35a6f5ed075bb
Srinivasan Sekar <srinivasan.sekar1990@gmail.com>
2020-06-28 12:31:54 +0530

    add angular library test

--- apps/angular-testing/src/app/app.component.spec.ts
+++ apps/angular-testing/src/app/app.component.spec.ts
@@ -1,16 +1,36 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
-import { render, screen, userEvent } from '@testing-library/angular';
-import { AppComponent } from './app.component';
+import { render, screen, userEvent, fireEvent } from '@testing-library/angular';
+import { AppComponent, Album } from './app.component';
+import { AlbumService } from './album.service';
+import { of } from 'rxjs';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 it('should search on username', async () => {
+  const albums: Album[] = [
+    {
+      userId: 1,
+      id: 1,
+      title: 'sarah',
+    }
+  ];
+
   await render(AppComponent, {
     imports: [HttpClientModule, ReactiveFormsModule],
+    componentProviders: [
+      {
+        provide: AlbumService,
+        useValue: {
+          getAlbums() {
+            return of(albums);
+          },
+        },
+      },
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
   });
 
-  const textbox = screen.getByPlaceholderText('Username');
+  fireEvent.click(screen.getByRole('button', { name: /get albums/i }));
 
-  await userEvent.type(textbox, 'timdeschryver');
-
-  await screen.findByText(/mocked-timdeschryver/i);
+  await screen.findByText(/sarah/i);
 });
